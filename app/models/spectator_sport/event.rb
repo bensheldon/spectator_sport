7523ba695acea8d2ca441e6e7a42419983d133c7
Jan Peterka <jan.peterka@hey.com>
2024-09-29 19:24:06 +0200

    Move enums to constants, improve showing click

--- app/models/spectator_sport/event.rb
+++ app/models/spectator_sport/event.rb
@@ -5,36 +5,51 @@ module SpectatorSport
 
     Explanation = Struct.new(:title, :details)
 
+    # taken from https://github.com/rrweb-io/rrweb/blob/9488deb6d54a5f04350c063d942da5e96ab74075/src/types.ts
+    EVENT_TYPES = %w[DomContentLoaded Load FullSnapshot IncrementalSnapshot Meta Custom]
+
+    EVENT_SOURCES = %w[Mutation MouseMove MouseInteraction Scroll ViewportResize Input TouchMove MediaInteraction
+                       StyleSheetRule CanvasMutation Font Log Drag StyleDeclaration Selection AdoptedStyleSheet]
+
+    MOUSE_INTERACTIONS = %w[MouseUp MouseDown Click ContextMenu DblClick Focus Blur TouchStart TouchMove_Departed
+                            TouchEnd TouchCancel]
+
     def explanation
-      # taken from https://github.com/rrweb-io/rrweb/blob/9488deb6d54a5f04350c063d942da5e96ab74075/src/types.ts#L10
-      event_types = %w[DomContentLoaded Load FullSnapshot IncrementalSnapshot Meta Custom]
-      event_type = event_types[event_data["type"]]
-
-      explanation = Explanation.new(event_type, [])
-
-      event_sources = %w[Mutation MouseMove MouseInteraction Scroll ViewportResize Input TouchMove MediaInteraction
-                        StyleSheetRule CanvasMutation Font Log Drag StyleDeclaration Selection AdoptedStyleSheet]
+      explanation = Explanation.new(title, [])
 
       if event_type == "Meta"
         explanation.details << "visited #{event_data.dig("data", "href")}"
       end
 
-      if event_type == "IncrementalSnapshot"
-        event_source = event_sources[event_data.dig("data", "source")]
+      if event_source
         explanation.details << "source: #{event_source}"
-      else
-        event_source = nil
       end
 
-      mouse_interactions =%w[MouseUp MouseDown Click ContextMenu DblClick Focus Blur TouchStart TouchMove_Departed
-                              TouchEnd TouchCancel]
-
       if event_source == "MouseInteraction"
-        mouse_interaction = mouse_interactions[event_data.dig("data", "type")]
+        mouse_interaction = MOUSE_INTERACTIONS[event_data.dig("data", "type")]
         explanation.details << "#{mouse_interaction}"
       end
 
       explanation
+    end
+
+    def event_type
+      EVENT_TYPES[event_data["type"]]
+    end
+
+    def event_source
+      return unless event_data.dig("data", "source")
+
+      EVENT_SOURCES[event_data.dig("data", "source")]
+    end
+
+    def title
+      if event_type == "IncrementalSnapshot" && event_source == "MouseInteraction" &&
+          MOUSE_INTERACTIONS[event_data.dig("data", "type")] == "Click"
+        "Mouse Click"
+      else
+        event_type
+      end
     end
 
     def page
