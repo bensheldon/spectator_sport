b67233a1e45dac8baee93c1b38264e19064fb35a
Ben Sheldon [he/him] <bensheldon@gmail.com>
2025-07-30 15:21:50 -0700

    Store and restore player seconds in URL hash to make reloading player easier; update to rrweb 2.0.0-alpha.18

--- app/frontend/spectator_sport/dashboard/modules/player_controller.js
+++ app/frontend/spectator_sport/dashboard/modules/player_controller.js
@@ -5,6 +5,7 @@ import { Player } from  'rrweb-player';
 export default class extends Controller {
   static values = {
     events: { type: Array, default: [] },
+    events_after: { type: Number, default: 0 },
   }
 
   connect() {
@@ -12,7 +13,18 @@ export default class extends Controller {
       target: this.element,
       props: {
         events: this.eventsValue,
+        liveMode: true,
       }
+    });
+
+    if (window.location.hash) {
+      const seconds = parseInt(window.location.hash.substring(1));
+      this.player.getReplayer().play(seconds * 1000);
+    }
+
+    this.player.addEventListener('ui-update-current-time', (event) => {
+      const seconds = parseInt(event.payload / 1000);
+      window.location.hash = seconds;
     });
 
     const playerElement = this.element.getElementsByClassName("rr-player")[0];
