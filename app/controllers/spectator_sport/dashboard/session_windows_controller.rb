460d1c8c47ad1cbdf0470c0c22efd9cd9f27ec32
Ben Sheldon [he/him] <bensheldon@gmail.com>
2025-09-05 13:22:17 -0700

    Paginate events loading on SessionWindow#show

--- app/controllers/spectator_sport/dashboard/session_windows_controller.rb
+++ app/controllers/spectator_sport/dashboard/session_windows_controller.rb
@@ -3,6 +3,18 @@ module SpectatorSport
     class SessionWindowsController < ApplicationController
       def show
         @session_window = SessionWindow.find(params[:id])
+        @events = @session_window.events.page_after(nil)
+      end
+
+      def events
+        response.content_type = "text/vnd.turbo-stream.html"
+        return head(:ok) if params[:after_event_id].blank?
+
+        session_window = SessionWindow.find(params[:id])
+        previous_event = session_window.events.find_by(id: params[:after_event_id])
+        @events = session_window.events.page_after(previous_event)
+
+        render layout: false
       end
 
       def destroy
--- app/models/spectator_sport/event.rb
+++ app/models/spectator_sport/event.rb
@@ -1,6 +1,10 @@
 module SpectatorSport
   class Event < ApplicationRecord
+    PAGE_LIMIT = 1_000
+
     belongs_to :session
     belongs_to :session_window
+
+    scope :page_after, ->(event) { (event ? where("(created_at, id) > (?, ?)", event.created_at, event.id) : self).order(created_at: :asc, id: :asc).limit(PAGE_LIMIT) }
   end
 end
